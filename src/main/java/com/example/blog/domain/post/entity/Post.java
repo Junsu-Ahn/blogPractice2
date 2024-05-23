54cdf9e97cbb4a837f95fc6d2c309909c3657403
Junsu-Ahn <ajunsu5@gmail.com>
2024-05-23 17:27:21 +0900

    Member 구현

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -1,15 +1,26 @@
 package com.example.blog.domain.post.entity;
 
+import com.example.blog.domain.member.entity.Member;
 import com.example.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class Post extends BaseEntity {
 
     private String title;
     private String content;
+    @ManyToOne
+    private Member author;
 }
