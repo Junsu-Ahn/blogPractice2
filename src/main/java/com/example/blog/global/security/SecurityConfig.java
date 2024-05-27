f83871d342dcf2fef699dd8cf79385812eeea3cf
Junsu-Ahn <ajunsu5@gmail.com>
2024-05-27 15:48:13 +0900

    kakao login complete

--- src/main/java/com/example/blog/global/security/SecurityConfig.java
+++ src/main/java/com/example/blog/global/security/SecurityConfig.java
@@ -25,6 +25,11 @@ public class SecurityConfig {
                         formLogin -> formLogin
                                 .loginPage("/member/login")
                                 .defaultSuccessUrl("/")
+
+                )
+                .oauth2Login(
+                        oauth2Login -> oauth2Login
+                                .loginPage("/member/login")
                 )
                 .logout(
                         logout -> logout
